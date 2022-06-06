92ac28fb64277dbbefe0389a9341d1ea9494ba41
Bernard Leanse <bernardleanse96@gmail.com>
2022-06-06 14:51:58 +0100

    first test passes

--- test_fizz_buzz.py
+++ test_fizz_buzz.py
@@ -1,5 +1,7 @@
 import unittest
+from fizz_buzz import FizzBuzz
 
 class TestFizzBuzz(unittest.TestCase):
-  def test_returns_fizz_for_three_multiples(self):
-    self.assertEqual(FizzBuzz.fb(3), "Fizz")
+  def test_returns_fizz_for_three(self):
+    fizz_buzz = FizzBuzz()
+    self.assertEqual(fizz_buzz.run(3), "fizz")
